94b01bf2e9665d7a8174ebbf48039808f853ca05
imprasadpatil <patilprasad737@gmail.com>
2022-10-20 18:59:14 +0530

    changes

--- API/API_Document.md
+++ API/API_Document.md
@@ -1,5 +1,20 @@
 #list of Product type with respect to Gender(Men/Women)
+https://myecommapi.herokuapp.com/producttype?genderid=2
 
-#list of Product Categories
+#list of Product with respect to Gender(Men/Women)
+https://myecommapi.herokuapp.com/product?genderid=2
 
-#list of Product with respect to New Arriwal
+#list of Product with respect to Gender(Men/Women) and Product type
+https://myecommapi.herokuapp.com/product/2?producttypeid=6
+
+#list of Product with respect to New Arriwal and Gender(Men/Women)
+https://myecommapi.herokuapp.com/product/newarr
+
+#list of Product type
+https://myecommapi.herokuapp.com/producttype
+
+#product details with respect to product id
+https://myecommapi.herokuapp.com/product/detail/20
+
+
+//https://myecommapi.herokuapp.com/product
--- API/app.js
+++ API/app.js
@@ -25,19 +25,111 @@ app.get('/',(req,res) => {
     res.send('Express App Is Running')
 })
 
-// list of city
-app.get('/product',(req,res) => {
-    let query = {};
-    let genderId = Number(req.query.genderId);
-    if(genderId){
-        query={gender_id:genderId}
+// list of gender
+app.get('/gender',(req,res) => {
+    let query = {}
+    let genderid = Number(req.query.genderid)
+    if(genderid){
+        query = {gender_id:genderid}
     }
-    db.collection('product').find().toArray((err,result)=>{
+    db.collection('gender').find(query).toArray((err,result)=>{
         if(err) throw err;
         res.send(result)
     })
 })
 
+//list id product type with respect to gender
+app.get('/producttype',(req,res) => {
+    let query = {}
+    let genderid =Number(req.query.genderid)
+    if(genderid){
+        query = {gender_id:genderid}
+    }
+    db.collection('product_type').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//list id product with respect to gender
+app.get('/product',(req,res) => {
+    let query = {}
+    let genderid = Number(req.query.genderid)
+    if(genderid){
+        query = {gender_id:genderid}
+    }  
+    db.collection('product').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//product for gender with product type
+app.get('/product/:genderid/',(req,res) => {
+    let query = {}
+    let genderid = Number(req.params.genderid)
+    let producttypeid = Number(req.query.producttypeid)
+    if(producttypeid){
+        query = {gender_id:genderid,
+           product_type_id: producttypeid}
+    }  
+    db.collection('product').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//product details with respect to product id
+app.get('/product/detail/:productid',(req,res)=>{
+    let query = {}
+    let productid = Number(req.params.productid)
+    if(productid){
+        query = {product_id:productid}
+    }
+    db.collection('product').find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//get orders
+app.get('/getorder',(req,res) => {
+    
+    db.collection('orders').find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//placeorders
+app.post('/placeOrder',(req,res) => {
+    console.log(req.body);
+    db.collection('orders').insert(req.body,(err,result) => {
+        if(err) throw err;
+        res.send('Order Placed')
+        
+    })
+})
+//Update Order
+app.put('/updateOrder/:id',(req,res) => {
+    let oid = Number(req.params.id);
+    db.collection('orders').updateOne(
+        {orderId:oid},
+        {
+            $set:{
+                "status":req.body.status,
+                "bank_name":req.body.bank_name,
+                "date":req.body.date
+            }
+        },(err,result) => {
+            if(err) throw err;
+            res.send('Order Updated')
+        }
+    )
+})
+//delet orders
+app.delete('/deleteOrder/:id',(req,res) => {
+    let _id = mongo.ObjectId(req.params.id);
+    db.collection('orders').deleteOne({_id},(err,result) => {
+        if(err) throw err;
+        res.send('Order Deleted')
+    })
+})
 //connection with mongo
 MongoClient.connect(mongoUrl,(err,client)=>{
     if(err) console.log(`Error while connecting`);
